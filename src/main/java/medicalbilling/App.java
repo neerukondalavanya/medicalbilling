d9f9c7e5123b22a4bbb28eb47d7dfe781f7b6858
lavanya <chlavanya@acc.com>
2021-05-02 21:55:14 +0530

    dev developed medical ipbilling

---
+++ src/main/java/medicalbilling/App.java
@@ -0,0 +1,13 @@
+package medicalbilling;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
